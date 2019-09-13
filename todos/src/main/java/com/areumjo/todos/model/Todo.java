e4058bb857a7612c19d7b554aaff935e4cc8b573
Areum Jo <areumjo1@gmail.com>
2019-09-13 09:01:16 -0700

    Created models and abstract class auditable

---
+++ todos/src/main/java/com/areumjo/todos/model/Todo.java
@@ -0,0 +1,85 @@
+package com.areumjo.todos.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "todos")
+public class Todo extends Auditable
+{
+    // todoid primary key, not null long
+    // description string, not null
+    // datestarted datetime
+    // completed boolean
+    // userid foreign key (one user to many todos) not null
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String description;
+
+    private Date datestarted;
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "userid",
+            nullable = false)
+    @JsonIgnoreProperties({"quotes", "hibernateLazyInitializer"})
+    private User user;
+
+    public Todo() {
+    }
+
+    public Todo(String description, Date datestarted, boolean completed, User user) {
+        this.description = description;
+        this.datestarted = datestarted;
+        this.completed = completed;
+        this.user = user;
+    }
+
+    // getter and setter
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDatestarted() {
+        return datestarted;
+    }
+
+    public void setDatestarted(Date datestarted) {
+        this.datestarted = datestarted;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ todos/src/main/java/com/areumjo/todos/model/User.java
@@ -0,0 +1,91 @@
+package com.areumjo.todos.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User extends Auditable
+{
+
+    // userid primary key, not null long
+    // username string, not null unique
+    // password string, not null
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @Column(nullable = false,
+            unique = true)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("user")
+    private List<UserRoles> userRoles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    @JsonIgnoreProperties("user")
+    private List<Todo> todos = new ArrayList<>();
+
+    public User() {
+    }
+
+
+    public User(String username, String password, List<UserRoles> userRoles, List<Todo> todos) {
+        this.username = username;
+        this.password = password;
+        this.userRoles = userRoles;
+        this.todos = todos;
+    }
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(long userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<UserRoles> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoles> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<Todo> todos) {
+        this.todos = todos;
+    }
+}
